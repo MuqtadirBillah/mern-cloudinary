04d80c5a176ccbf792daee31eb101f9b84047090
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-11-10 00:14:08 +0500

    progress

--- multer-mern/multer/backend/controllers/fileController.js
+++ multer-mern/multer/backend/controllers/fileController.js
@@ -1,10 +1,27 @@
 const moment = require("moment");
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const fileUpload = (req, res)=>{
-    console.log(req.file);
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.cloud_api_key, 
+  api_secret: process.env.cloud_api_secret
+});
+
+const fileUpload = async (req, res)=>{
+    // console.log(working);
+    // console.log(req.file);
     try{
-        res.send({ status: "success", message: `${req.file.originalname} uploaded!` })
+        if(req.file){
+            const result = await cloudinary.uploader.upload(req.file.path);
+            console.log(result)
+            res.send({ status: "success", message: `${req.file.originalname} uploaded!` })
+        }
+        else{
+            res.send({ status: "error", message: `File not found!` })
+        }
     } catch(err){
+        console.log(err)
         res.send({ status: "err", error: err })
     }
 }
--- multer-mern/multer/backend/routes/fileRoutes.js
+++ multer-mern/multer/backend/routes/fileRoutes.js
@@ -2,18 +2,25 @@ const express = require("express");
 const fileController = require("../controllers/fileController")
 const fileRouter = express.Router();
 const multer  = require('multer')
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      let extArray = file.mimetype.split("/");
-      let extension = extArray[extArray.length - 1];
-      cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
-    }
-  })
-const upload = multer({ storage: storage })
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.cloud_api_key, 
+  api_secret: process.env.cloud_api_secret
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "DEV",
+    allowedFormats: ['png', 'jpg', 'jpeg'],
+    width: 250, height: 250, crop: "fill"
+  },
+});
+
+const upload = multer({ storage: storage });
 
 fileRouter.route("/upload").post(upload.single('image'), fileController.fileUpload);
 
