1de69051a2b4ae08df49d699851531e678884f60
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-11-10 18:19:16 +0500

    remove few console logs

--- multer-mern/multer/backend/controllers/fileController.js
+++ multer-mern/multer/backend/controllers/fileController.js
@@ -2,12 +2,6 @@ const moment = require("moment");
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const {
-    cloud_name,
-    cloud_api_key,
-    cloud_api_secret
-} = process.env;
-
 cloudinary.config({ 
   cloud_name: process.env.cloud_name, 
   api_key: process.env.cloud_api_key, 
@@ -15,16 +9,9 @@ cloudinary.config({
 });
 
 const fileUpload = async (req, res)=>{
-
-    console.log(cloud_name)
-    console.log(cloud_api_key)
-    console.log(cloud_api_secret)
-    console.log('working');
-    console.log(req.file);
     try{
         if(req.file){
             const result = await cloudinary.uploader.upload(req.file.path);
-            console.log(result)
             res.status(200).send({ status: "success", message: `${req.file.originalname} uploaded!` })
         }
         else{
